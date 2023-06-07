5d96b8d2d52d0fd31e38b81dd8b5996887a19148
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2023-06-07 13:14:55 -0300

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -2,45 +2,40 @@ const $home = document.getElementById("home");
 
 export const crearCard = (data) => {
   if (Array.isArray(data)) {
-    $home.appendChild(mostrarObjetosEnDiv(data));
-  }
-};
+    const divContenedor = document.createElement("div");
+    divContenedor.classList.add("centrado");
 
-const mostrarObjetosEnDiv = (data) => {
-  const divContenedor = document.createElement("div");
-  divContenedor.classList.add("centrado");
-  
-  const imagenes = {
-    fuerza: "/img/management.png",
-    alias: "/img/detective.png",
-    arma: "/img/fighting.png",
-    editorial: "/img/comic-book.png",
-  };
-  
-  data.forEach((objeto) => {
-    const parrafo = document.createElement("p");
-    const article = document.createElement("article");
-    article.classList.add("contenedor");
-    for (const key in objeto) {
-      if (key === "id") continue;
-      
-      const p = document.createElement("p");
-      p.classList.add("texto");
-      p.textContent = key + ": " + objeto[key];
-      parrafo.appendChild(p)
-      article.appendChild(parrafo);
+    const imagenes = {
+      fuerza: "/img/management.png",
+      alias: "/img/detective.png",
+      arma: "/img/fighting.png",
+      editorial: "/img/comic-book.png",
+    };
 
-      if (key in imagenes) {
-        const img = document.createElement("img");
-        img.src = imagenes[key];
-        img.classList.add("icons");
-        parrafo.appendChild(img)
-        article.appendChild(parrafo);
+    data.forEach((objeto) => {
+      const article = document.createElement("article");
+      article.classList.add("contenedor");
+
+      for (const key in objeto) {
+        if (key === "id") continue;
+
+        const p = document.createElement("p");
+        p.classList.add("texto");
+        p.textContent = key + ": " + objeto[key];
+
+        if (key in imagenes) {
+          const img = document.createElement("img");
+          img.src = imagenes[key];
+          img.classList.add("icons");
+          p.appendChild(img);
+        }
+
+        article.appendChild(p);
       }
-    }
 
-    divContenedor.appendChild(article);
-  });
+      divContenedor.appendChild(article);
+    });
 
-  return divContenedor;
+    $home.appendChild(divContenedor);
+  }
 };
